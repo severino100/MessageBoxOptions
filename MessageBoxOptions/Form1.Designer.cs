df2e339ca7d8b680eb8e753817a4cefc2fa5a3d9
rubenfranciscos <rubenfranciscos@gmail.com>
2015-11-18 02:50:19 +0000

    add

---
+++ MessageBoxOptions/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace MessageBoxOptions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuString = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuTitulo = new System.Windows.Forms.ToolStripMenuItem();
+            this.butoesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuButoesOK = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAbortRetryIgnore = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuButoesOKCancel = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuRetryCancel = new System.Windows.Forms.ToolStripMenuItem();
+            this.menButoesSimNao = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuButoesSimNaoCancelar = new System.Windows.Forms.ToolStripMenuItem();
+            this.iconsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuIconsInformacao = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuIconsErro = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuIconsNenhum = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuIconsPergunta = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuIconsAviso = new System.Windows.Forms.ToolStripMenuItem();
+            this.atalhoEtcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPosicaoDosObjetos = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuString,
+            this.menuTitulo,
+            this.butoesToolStripMenuItem,
+            this.iconsToolStripMenuItem,
+            this.menuPosicaoDosObjetos});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuString
+            // 
+            this.menuString.Name = "menuString";
+            this.menuString.Size = new System.Drawing.Size(50, 20);
+            this.menuString.Text = "String";
+            this.menuString.Click += new System.EventHandler(this.menuString_Click);
+            // 
+            // menuTitulo
+            // 
+            this.menuTitulo.Name = "menuTitulo";
+            this.menuTitulo.Size = new System.Drawing.Size(53, 20);
+            this.menuTitulo.Text = "Titulo ";
+            this.menuTitulo.Click += new System.EventHandler(this.menuTitulo_Click);
+            // 
+            // butoesToolStripMenuItem
+            // 
+            this.butoesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuButoesOK,
+            this.menuAbortRetryIgnore,
+            this.menuButoesOKCancel,
+            this.menuRetryCancel,
+            this.menButoesSimNao,
+            this.menuButoesSimNaoCancelar,
+            this.atalhoEtcToolStripMenuItem});
+            this.butoesToolStripMenuItem.Name = "butoesToolStripMenuItem";
+            this.butoesToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.butoesToolStripMenuItem.Text = "Butoes";
+            // 
+            // menuButoesOK
+            // 
+            this.menuButoesOK.Name = "menuButoesOK";
+            this.menuButoesOK.Size = new System.Drawing.Size(180, 22);
+            this.menuButoesOK.Text = "OK";
+            this.menuButoesOK.Click += new System.EventHandler(this.menuButoesOK_Click);
+            // 
+            // menuAbortRetryIgnore
+            // 
+            this.menuAbortRetryIgnore.Name = "menuAbortRetryIgnore";
+            this.menuAbortRetryIgnore.Size = new System.Drawing.Size(180, 22);
+            this.menuAbortRetryIgnore.Text = "AbortRetryIgnore";
+            this.menuAbortRetryIgnore.Click += new System.EventHandler(this.menuAbortRetryIgnore_Click);
+            // 
+            // menuButoesOKCancel
+            // 
+            this.menuButoesOKCancel.Name = "menuButoesOKCancel";
+            this.menuButoesOKCancel.Size = new System.Drawing.Size(180, 22);
+            this.menuButoesOKCancel.Text = "OK e Cancel";
+            this.menuButoesOKCancel.Click += new System.EventHandler(this.menuButoesOKCancel_Click);
+            // 
+            // menuRetryCancel
+            // 
+            this.menuRetryCancel.Name = "menuRetryCancel";
+            this.menuRetryCancel.Size = new System.Drawing.Size(180, 22);
+            this.menuRetryCancel.Text = "RetryCancel";
+            this.menuRetryCancel.Click += new System.EventHandler(this.menuRetryCancel_Click);
+            // 
+            // menButoesSimNao
+            // 
+            this.menButoesSimNao.Name = "menButoesSimNao";
+            this.menButoesSimNao.Size = new System.Drawing.Size(180, 22);
+            this.menButoesSimNao.Text = "Sim e Nao";
+            this.menButoesSimNao.Click += new System.EventHandler(this.menButoesSimNao_Click);
+            // 
+            // menuButoesSimNaoCancelar
+            // 
+            this.menuButoesSimNaoCancelar.Name = "menuButoesSimNaoCancelar";
+            this.menuButoesSimNaoCancelar.Size = new System.Drawing.Size(180, 22);
+            this.menuButoesSimNaoCancelar.Text = "Sim, Nao e Cancelar";
+            this.menuButoesSimNaoCancelar.Click += new System.EventHandler(this.menuButoesSimNaoCancelar_Click);
+            // 
+            // iconsToolStripMenuItem
+            // 
+            this.iconsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuIconsInformacao,
+            this.menuIconsErro,
+            this.menuIconsNenhum,
+            this.menuIconsPergunta,
+            this.menuIconsAviso});
+            this.iconsToolStripMenuItem.Name = "iconsToolStripMenuItem";
+            this.iconsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.iconsToolStripMenuItem.Text = "Icons";
+            // 
+            // menuIconsInformacao
+            // 
+            this.menuIconsInformacao.Name = "menuIconsInformacao";
+            this.menuIconsInformacao.Size = new System.Drawing.Size(152, 22);
+            this.menuIconsInformacao.Text = "Informaçao";
+            this.menuIconsInformacao.Click += new System.EventHandler(this.menuIconsInformacao_Click);
+            // 
+            // menuIconsErro
+            // 
+            this.menuIconsErro.Name = "menuIconsErro";
+            this.menuIconsErro.Size = new System.Drawing.Size(152, 22);
+            this.menuIconsErro.Text = "Erro";
+            this.menuIconsErro.Click += new System.EventHandler(this.menuIconsErro_Click);
+            // 
+            // menuIconsNenhum
+            // 
+            this.menuIconsNenhum.Name = "menuIconsNenhum";
+            this.menuIconsNenhum.Size = new System.Drawing.Size(152, 22);
+            this.menuIconsNenhum.Text = "Nenhum";
+            this.menuIconsNenhum.Click += new System.EventHandler(this.menuIconsNenhum_Click);
+            // 
+            // menuIconsPergunta
+            // 
+            this.menuIconsPergunta.Name = "menuIconsPergunta";
+            this.menuIconsPergunta.Size = new System.Drawing.Size(152, 22);
+            this.menuIconsPergunta.Text = "Pergunta";
+            this.menuIconsPergunta.Click += new System.EventHandler(this.menuIconsPergunta_Click);
+            // 
+            // menuIconsAviso
+            // 
+            this.menuIconsAviso.Name = "menuIconsAviso";
+            this.menuIconsAviso.Size = new System.Drawing.Size(152, 22);
+            this.menuIconsAviso.Text = "Aviso";
+            this.menuIconsAviso.Click += new System.EventHandler(this.menuIconsAviso_Click);
+            // 
+            // atalhoEtcToolStripMenuItem
+            // 
+            this.atalhoEtcToolStripMenuItem.Name = "atalhoEtcToolStripMenuItem";
+            this.atalhoEtcToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.atalhoEtcToolStripMenuItem.Text = "Atalho etc";
+            this.atalhoEtcToolStripMenuItem.Click += new System.EventHandler(this.atalhoEtcToolStripMenuItem_Click);
+            // 
+            // menuPosicaoDosObjetos
+            // 
+            this.menuPosicaoDosObjetos.Name = "menuPosicaoDosObjetos";
+            this.menuPosicaoDosObjetos.Size = new System.Drawing.Size(126, 20);
+            this.menuPosicaoDosObjetos.Text = "Posicao dos Objetos";
+            this.menuPosicaoDosObjetos.Click += new System.EventHandler(this.menuPosicaoDosObjetos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(462, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Opçoes na MessageBox.Show";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuString;
+        private System.Windows.Forms.ToolStripMenuItem menuTitulo;
+        private System.Windows.Forms.ToolStripMenuItem butoesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuButoesOK;
+        private System.Windows.Forms.ToolStripMenuItem menuAbortRetryIgnore;
+        private System.Windows.Forms.ToolStripMenuItem menuButoesOKCancel;
+        private System.Windows.Forms.ToolStripMenuItem menuRetryCancel;
+        private System.Windows.Forms.ToolStripMenuItem menButoesSimNao;
+        private System.Windows.Forms.ToolStripMenuItem menuButoesSimNaoCancelar;
+        private System.Windows.Forms.ToolStripMenuItem iconsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuIconsInformacao;
+        private System.Windows.Forms.ToolStripMenuItem menuIconsErro;
+        private System.Windows.Forms.ToolStripMenuItem menuIconsNenhum;
+        private System.Windows.Forms.ToolStripMenuItem menuIconsPergunta;
+        private System.Windows.Forms.ToolStripMenuItem menuIconsAviso;
+        private System.Windows.Forms.ToolStripMenuItem atalhoEtcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuPosicaoDosObjetos;
+    }
+}
+
---
+++ MessageBoxOptions/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxOptions
+{
+    public partial class Form1 : Form
+    {
+        public string caminhoBotaoComLigacao = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void menuString_Click(object sender, EventArgs e)
+        {
+            // String
+            //Com isto nós pretendemos apaenas mostrar uma message box com uma string
+            MessageBox.Show("Ola eu sou uma sting inutil");
+            
+        }
+
+        private void menuTitulo_Click(object sender, EventArgs e)
+        {
+            // Titulo
+            //Com isto nós pretendemos mostrar uma message box com uma string mas com um titulo
+            MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos");
+        }
+
+
+        //MessageBox.Show(this, " ms ", "caption", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button3);
+        
+        
+        
+        
+        
+        private void menuButoesOK_Click(object sender, EventArgs e)
+        {
+            /* Botao OK
+                Aqui queremos que a nossa message box contenha um botao ok  */
+            DialogResult result = MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.OK);
+
+             switch (result)
+             {
+                 case DialogResult.Abort:
+                     MessageBox.Show("Voce abortou");
+                     break;
+                 case DialogResult.Cancel:
+                     MessageBox.Show("Voce cancelou");
+                     break;
+                 case DialogResult.Ignore:
+                     MessageBox.Show("Voce ignorou");
+                     break;
+                 case DialogResult.No:
+                     MessageBox.Show("NOOOO");
+                     break;
+                 case DialogResult.None:
+                     MessageBox.Show("None");
+                     break;
+                 case DialogResult.Retry:
+                     MessageBox.Show("Retry");
+                     break;
+                 case DialogResult.Yes:
+                     MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                     break;
+                 case DialogResult.OK:
+                     MessageBox.Show("cLICOU NO OK");
+                     break;
+             }
+        
+        }
+
+        private void menuAbortRetryIgnore_Click(object sender, EventArgs e)
+        {
+            /* Botao Abort Retry e Ignore
+                Aqui queremos que a nossa message box contenha 3 butoes com varias ações  */
+            DialogResult result = MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.AbortRetryIgnore);
+
+             switch (result)
+             {
+                 case DialogResult.Abort:
+                     MessageBox.Show("Voce abortou");
+                     break;
+                 case DialogResult.Cancel:
+                     MessageBox.Show("Voce cancelou");
+                     break;
+                 case DialogResult.Ignore:
+                     MessageBox.Show("Voce ignorou");
+                     break;
+                 case DialogResult.No:
+                     MessageBox.Show("NOOOO");
+                     break;
+                 case DialogResult.None:
+                     MessageBox.Show("None");
+                     break;
+                 case DialogResult.Retry:
+                     MessageBox.Show("Retry");
+                     break;
+                 case DialogResult.Yes:
+                     MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                     break;
+                 case DialogResult.OK:
+                     MessageBox.Show("cLICOU NO OK");
+                     break;
+             }
+        }
+
+        private void menuButoesOKCancel_Click(object sender, EventArgs e)
+        {
+            /* Botao OK e Cancel
+                Aqui queremos que a nossa message box contenha 2 butoes com varias ações  */
+            DialogResult result = MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.OKCancel);
+
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    MessageBox.Show("Voce abortou");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("Voce cancelou");
+                    break;
+                case DialogResult.Ignore:
+                    MessageBox.Show("Voce ignorou");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("NOOOO");
+                    break;
+                case DialogResult.None:
+                    MessageBox.Show("None");
+                    break;
+                case DialogResult.Retry:
+                    MessageBox.Show("Retry");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                    break;
+                case DialogResult.OK:
+                    MessageBox.Show("cLICOU NO OK");
+                    break;
+            }
+        }
+
+        private void menuRetryCancel_Click(object sender, EventArgs e)
+        {
+            /* Botao Abort Retry e Cancel
+                Aqui queremos que a nossa message box contenha 2 butoes com varias ações  */
+            DialogResult result = MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.RetryCancel);
+
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    MessageBox.Show("Voce abortou");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("Voce cancelou");
+                    break;
+                case DialogResult.Ignore:
+                    MessageBox.Show("Voce ignorou");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("NOOOO");
+                    break;
+                case DialogResult.None:
+                    MessageBox.Show("None");
+                    break;
+                case DialogResult.Retry:
+                    MessageBox.Show("Retry");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                    break;
+                case DialogResult.OK:
+                    MessageBox.Show("cLICOU NO OK");
+                    break;
+            }
+        }
+
+        private void menButoesSimNao_Click(object sender, EventArgs e)
+        {
+            /* Botao Abort Sim e Nao
+                Aqui queremos que a nossa message box contenha 2 butoes com varias ações  */
+            DialogResult result = MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.YesNo);
+
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    MessageBox.Show("Voce abortou");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("Voce cancelou");
+                    break;
+                case DialogResult.Ignore:
+                    MessageBox.Show("Voce ignorou");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("NOOOO");
+                    break;
+                case DialogResult.None:
+                    MessageBox.Show("None");
+                    break;
+                case DialogResult.Retry:
+                    MessageBox.Show("Retry");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                    break;
+                case DialogResult.OK:
+                    MessageBox.Show("cLICOU NO OK");
+                    break;
+            }
+        }
+
+        private void menuButoesSimNaoCancelar_Click(object sender, EventArgs e)
+        {
+            /* Botao Sim Nao e Cancel
+                Aqui queremos que a nossa message box contenha 3 butoes com varias ações  */
+            DialogResult result = MessageBox.Show("Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    MessageBox.Show("Voce abortou");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("Voce cancelou");
+                    break;
+                case DialogResult.Ignore:
+                    MessageBox.Show("Voce ignorou");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("NOOOO");
+                    break;
+                case DialogResult.None:
+                    MessageBox.Show("None");
+                    break;
+                case DialogResult.Retry:
+                    MessageBox.Show("Retry");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                    break;
+                case DialogResult.OK:
+                    MessageBox.Show("cLICOU NO OK");
+                    break;
+            }
+        }
+
+        private void atalhoEtcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(this, 
+                                    "Ola eu sou uma sting inutil", 
+                                    "Programaçao para meninos", 
+                                    MessageBoxButtons.YesNoCancel,
+                                    MessageBoxIcon.Question,
+                                    MessageBoxDefaultButton.Button1, 0, 
+                                    @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    MessageBox.Show("Voce abortou");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("Voce cancelou");
+                    break;
+                case DialogResult.Ignore:
+                    MessageBox.Show("Voce ignorou");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("NOOOO");
+                    break;
+                case DialogResult.None:
+                    MessageBox.Show("None");
+                    break;
+                case DialogResult.Retry:
+                    MessageBox.Show("Retry");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("YEEEES FINALYYYYYYYY!!");
+                    break;
+                case DialogResult.OK:
+                    MessageBox.Show("cLICOU NO OK");
+                    break;
+            }
+        }
+
+        private void menuIconsInformacao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, 
+                            "Ola eu sou uma sting inutil", 
+                            "Programaçao para meninos", 
+                            MessageBoxButtons.YesNoCancel, 
+                            MessageBoxIcon.Information, 
+                            MessageBoxDefaultButton.Button1);
+        }
+
+        private void menuIconsErro_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void menuIconsNenhum_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.YesNoCancel, MessageBoxIcon.None, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void menuIconsPergunta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void menuIconsAviso_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Ola eu sou uma sting inutil", "Programaçao para meninos", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void menuPosicaoDosObjetos_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, 
+                "Ola eu sou uma sting inutil", 
+                "Programaçao para meninos", 
+                MessageBoxButtons.YesNoCancel, 
+                MessageBoxIcon.Information, 
+                MessageBoxDefaultButton.Button3, 
+                System.Windows.Forms.MessageBoxOptions.RtlReading);
+        } 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
